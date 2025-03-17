0d5b05184ada265e3d75abe661f56a93d4c2eae3
Isaque Vicoso <isaquevicoso@gmail.com>
2025-03-17 21:20:47 +0000

    Added better mobile funcionality

--- app.js
+++ app.js
@@ -165,10 +165,28 @@ async function showFilePreview(item) {
     previewContainer.innerHTML = '';
     
     if (extension === 'pdf') {
+        const previewWrapper = document.createElement('div');
+        previewWrapper.className = 'pdf-preview-wrapper';
+        
+        // Add download button for mobile
+        const downloadButton = document.createElement('a');
+        downloadButton.href = url;
+        downloadButton.download = item.name;
+        downloadButton.className = 'download-button';
+        downloadButton.innerHTML = `
+            <svg viewBox="0 0 20 20" fill="currentColor" class="download-icon">
+                <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+            Download PDF
+        `;
+        
         const iframe = document.createElement('iframe');
         iframe.className = 'pdf-preview';
         iframe.src = url;
-        previewContainer.appendChild(iframe);
+        
+        previewWrapper.appendChild(downloadButton);
+        previewWrapper.appendChild(iframe);
+        previewContainer.appendChild(previewWrapper);
     } else if (['jpg', 'jpeg', 'png', 'gif'].includes(extension)) {
         const img = document.createElement('img');
         img.className = 'image-preview';
